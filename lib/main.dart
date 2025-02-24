e15a87ab40f0ea24ded36d717085a7ffe25f56d0
emmarp <ruiizemmanuel@gmail.com>
2025-02-24 16:47:28 -0600

    Feat: Sora font added from Google Fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coffe_shop_app/src/screens/main_screen.dart';
 import 'package:coffe_shop_app/src/themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      theme: AppTheme.lightTheme,
+      theme: AppTheme.lightTheme.copyWith(
+        textTheme: GoogleFonts.soraTextTheme(Theme.of(context).textTheme),
+      ),
       home: const MainScreen(),
     );
   }
