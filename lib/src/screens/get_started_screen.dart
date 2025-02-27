346d5248068a60a8224d0ff7a8dd5ddf6cf8c712
emmarp <ruiizemmanuel@gmail.com>
2025-02-27 00:42:31 -0600

    Feat: Get Started Screen added

--- lib/src/screens/get_started_screen.dart
+++ lib/src/screens/get_started_screen.dart
@@ -1,17 +1,103 @@
+import 'package:coffe_shop_app/src/themes/light_theme.dart';
 import 'package:flutter/material.dart';
-
-void main() => runApp(const GetStartedScreen());
 
 class GetStartedScreen extends StatelessWidget {
   const GetStartedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
-        body: const Center(child: Text('Hello World')),
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Image.asset(
+                  "assets/get_started_background.png",
+                  fit: BoxFit.fitWidth,
+                  width: size.width,
+                ),
+              ),
+              Expanded(child: Container(color: Colors.black)),
+            ],
+          ),
+
+          Column(
+            children: [
+              Expanded(child: SizedBox()),
+              Expanded(
+                // flex: ,
+                child: Container(
+                  width: size.width,
+                  margin: const EdgeInsets.only(top: 100),
+                  decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(color: Colors.black, blurRadius: 30)],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      bottom: 30,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Fall in Love with Coffe in Blissful Delight!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 42,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                            height: 1.5,
+                            wordSpacing: 0.5,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          'Welcome to our cozy coffe corner, where every cup is a delightful for you.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: LightTheme.textLightColor,
+                            height: 1.5,
+                          ),
+                        ),
+                        Spacer(),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 56,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Acción al presionar el botón
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: const Text(
+                              'Get Started',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
