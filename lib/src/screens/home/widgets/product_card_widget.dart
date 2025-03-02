ba97fc97124dea81bacf39883267103de188ceb5
emmarp <ruiizemmanuel@gmail.com>
2025-03-02 12:24:14 -0600

    Feat: Product Card Widget to display a Product

---
+++ lib/src/screens/home/widgets/product_card_widget.dart
@@ -0,0 +1,79 @@
+import 'package:coffe_shop_app/src/themes/light_theme.dart';
+import 'package:flutter/material.dart';
+
+class ProductCardWidget extends StatelessWidget {
+  const ProductCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                'assets/coffee.jpg',
+                fit: BoxFit.cover,
+                height: 150,
+                width: double.infinity,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cappuccino',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Deep Foam',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: LightTheme.textLightColor,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$ 5.99',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: () {},
+                        minWidth: 0,
+                        padding: EdgeInsets.zero,
+                        elevation: 0,
+                        color: LightTheme.primaryColor,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Icon(Icons.add),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
