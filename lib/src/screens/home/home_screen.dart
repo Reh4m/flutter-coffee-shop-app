351e28977e82e2b4515abc7f588d5fae62cd4a22
emmarp <ruiizemmanuel@gmail.com>
2025-03-05 01:07:02 -0600

    Refactor Home screen layout using SingleChildScrollView widget

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:coffe_shop_app/src/screens/home/widgets/product_card_widget.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,26 +7,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(right: 25, left: 25),
-          child: Flexible(
-            child: GridView.builder(
-              itemCount: 10,
-              shrinkWrap: true,
-              padding: EdgeInsets.zero,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 15,
-                mainAxisExtent: 322,
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        dragStartBehavior: DragStartBehavior.down,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: GridView.builder(
+                itemCount: 10,
+                shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 15,
+                  mainAxisExtent: 322,
+                ),
+                itemBuilder: (context, index) {
+                  return ProductCardWidget();
+                },
               ),
-              itemBuilder: (context, index) {
-                return ProductCardWidget();
-              },
             ),
-          ),
+          ],
         ),
       ),
     );
