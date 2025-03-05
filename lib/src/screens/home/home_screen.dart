aaedd3bfb39d32a8b115e30762bb84b05f775458
emmarp <ruiizemmanuel@gmail.com>
2025-03-05 00:40:10 -0600

    Feat: Display example products in Home screen

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coffe_shop_app/src/screens/home/widgets/product_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(right: 25, left: 25),
+          child: Flexible(
+            child: GridView.builder(
+              itemCount: 10,
+              shrinkWrap: true,
+              padding: EdgeInsets.zero,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 15,
+                mainAxisExtent: 322,
+              ),
+              itemBuilder: (context, index) {
+                return ProductCardWidget();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
