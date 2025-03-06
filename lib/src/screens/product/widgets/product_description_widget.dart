f45f90593fe7c93c7ecff2fb66239118a3f5bfea
emmarp <ruiizemmanuel@gmail.com>
2025-03-06 06:16:23 -0600

    Chore: Read More widget added to Product Description

--- lib/src/screens/product/widgets/product_description_widget.dart
+++ lib/src/screens/product/widgets/product_description_widget.dart
@@ -1,5 +1,6 @@
 import 'package:coffe_shop_app/src/themes/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDescriptionWidget extends StatelessWidget {
   const ProductDescriptionWidget({super.key});
@@ -22,12 +23,29 @@ class ProductDescriptionWidget extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10),
-          Text(
+          ReadMoreText(
             'Caffe Mocha is a chocolate-flavored variant of a caffè latte. Other commonly used spellings are mochaccino and also mochachino. It is usually served with whipped cream, and topped with a dusting of either cocoa, cinnamon, or both.',
+            trimMode: TrimMode.Line,
+            trimLines: 3,
+            trimCollapsedText: 'Read More',
+            trimExpandedText: 'Show Less',
+            colorClickableText: LightTheme.primaryColor,
             style: const TextStyle(
               color: LightTheme.textLightColor,
               fontSize: 16,
-              fontWeight: FontWeight.w400,
+              fontWeight: FontWeight.w200,
+              height: 1.5,
+            ),
+            moreStyle: TextStyle(
+              color: LightTheme.primaryColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              height: 1.5,
+            ),
+            lessStyle: TextStyle(
+              color: LightTheme.primaryColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
               height: 1.5,
             ),
           ),
