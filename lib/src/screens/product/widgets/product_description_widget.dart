4596f279545fea53a7a488d2ce731b1dc786189a
emmarp <ruiizemmanuel@gmail.com>
2025-03-06 06:21:38 -0600

    Refactor (ProductDetail): Extract 'ProductDescriptionWidget' into a separate class

--- lib/src/screens/product/widgets/product_description_widget.dart
+++ lib/src/screens/product/widgets/product_description_widget.dart
@@ -14,43 +14,48 @@ class ProductDescriptionWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Description',
-            style: const TextStyle(
-              color: LightTheme.textColor,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
+          _titleWidget(),
           const SizedBox(height: 10),
-          ReadMoreText(
-            'Caffe Mocha is a chocolate-flavored variant of a caffè latte. Other commonly used spellings are mochaccino and also mochachino. It is usually served with whipped cream, and topped with a dusting of either cocoa, cinnamon, or both.',
-            trimMode: TrimMode.Line,
-            trimLines: 3,
-            trimCollapsedText: 'Read More',
-            trimExpandedText: 'Show Less',
-            colorClickableText: LightTheme.primaryColor,
-            style: const TextStyle(
-              color: LightTheme.textLightColor,
-              fontSize: 16,
-              fontWeight: FontWeight.w200,
-              height: 1.5,
-            ),
-            moreStyle: TextStyle(
-              color: LightTheme.primaryColor,
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              height: 1.5,
-            ),
-            lessStyle: TextStyle(
-              color: LightTheme.primaryColor,
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              height: 1.5,
-            ),
-          ),
+          _descriptionWidget(),
         ],
       ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      'Description',
+      style: TextStyle(
+        color: LightTheme.textColor,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _descriptionWidget() {
+    TextStyle readMoreTextStyle = const TextStyle(
+      color: LightTheme.primaryColor,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      height: 1.5,
+    );
+
+    return ReadMoreText(
+      'Caffe Mocha is a chocolate-flavored variant of a caffè latte. Other commonly used spellings are mochaccino and also mochachino. It is usually served with whipped cream, and topped with a dusting of either cocoa, cinnamon, or both.',
+      trimMode: TrimMode.Line,
+      trimLines: 3,
+      trimCollapsedText: 'Read More',
+      trimExpandedText: 'Show Less',
+      colorClickableText: LightTheme.primaryColor,
+      style: const TextStyle(
+        color: LightTheme.textLightColor,
+        fontSize: 16,
+        fontWeight: FontWeight.w200,
+        height: 1.5,
+      ),
+      moreStyle: readMoreTextStyle,
+      lessStyle: readMoreTextStyle,
     );
   }
 }
