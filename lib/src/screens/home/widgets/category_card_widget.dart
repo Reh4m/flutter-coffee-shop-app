9a3243a42237f99ebb4282ff002dac951ac45b4a
emmarp <ruiizemmanuel@gmail.com>
2025-03-09 22:55:37 -0600

    Feat: Change Category Card color based on selection state

--- lib/src/screens/home/widgets/category_card_widget.dart
+++ lib/src/screens/home/widgets/category_card_widget.dart
@@ -1,9 +1,11 @@
 import 'package:coffe_shop_app/src/models/category_model.dart';
+import 'package:coffe_shop_app/src/themes/light_theme.dart';
 import 'package:coffe_shop_app/src/themes/theme.dart';
 import 'package:flutter/material.dart';
 
 class CategoryCardWidget extends StatelessWidget {
   final Category category;
+  final int selectedCategory = 0;
 
   const CategoryCardWidget({super.key, required this.category});
 
@@ -14,7 +16,10 @@ class CategoryCardWidget extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(right: 20),
         child: Card(
-          color: AppTheme.lightTheme.primaryColor,
+          color:
+              category.id == selectedCategory
+                  ? AppTheme.lightTheme.primaryColor
+                  : Color(0xFFEDEDED),
           margin: const EdgeInsets.all(0),
           elevation: 0,
           shape: RoundedRectangleBorder(
@@ -26,8 +31,14 @@ class CategoryCardWidget extends StatelessWidget {
               child: Text(
                 category.name,
                 style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
+                  color:
+                      category.id == selectedCategory
+                          ? Colors.white
+                          : LightTheme.textColor,
+                  fontWeight:
+                      category.id == selectedCategory
+                          ? FontWeight.w600
+                          : FontWeight.w400,
                   fontSize: 16,
                 ),
               ),
