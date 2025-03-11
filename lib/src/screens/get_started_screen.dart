b32e2e645a90aeb5efaa0092de53f340971dcbad
emmarp <ruiizemmanuel@gmail.com>
2025-03-11 16:01:01 -0600

    Refactor: Using Positioned widget to build 'GetStartedScreen'

--- lib/src/screens/get_started_screen.dart
+++ lib/src/screens/get_started_screen.dart
@@ -12,91 +12,90 @@ class GetStartedScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Image.asset(
-                  "assets/get_started_background.png",
-                  fit: BoxFit.fitWidth,
-                  width: size.width,
+          Positioned(
+            top: 0,
+            left: 0,
+            bottom: size.height * 0.3,
+            child: Container(
+              height: size.height,
+              width: size.width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/get_started_background.png'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Expanded(child: Container(color: Colors.black)),
-            ],
+            ),
           ),
-
-          Column(
-            children: [
-              Expanded(child: SizedBox()),
-              Expanded(
-                // flex: ,
-                child: Container(
-                  width: size.width,
-                  margin: const EdgeInsets.only(top: 100),
-                  decoration: BoxDecoration(
-                    boxShadow: [BoxShadow(color: Colors.black, blurRadius: 30)],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                      bottom: 30,
+          Positioned(
+            top: size.height * 0.7,
+            child: Container(
+              height: size.height,
+              width: size.width,
+              decoration: const BoxDecoration(color: Colors.black),
+            ),
+          ),
+          Positioned(
+            top: 500,
+            bottom: 0,
+            child: Container(
+              height: size.height,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                boxShadow: const [
+                  BoxShadow(color: Colors.black, blurRadius: 100),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Fall in Love with Coffe in Blissful Delight!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        height: 1.5,
+                      ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Fall in Love with Coffe in Blissful Delight!',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 42,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                            height: 1.5,
-                            wordSpacing: 0.5,
-                          ),
-                        ),
-                        Spacer(),
-                        Text(
-                          'Welcome to our cozy coffe corner, where every cup is a delightful for you.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: LightTheme.textLightColor,
-                            height: 1.5,
-                          ),
-                        ),
-                        Spacer(),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 56,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context).push(_createRoute());
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor,
-                              foregroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            child: const Text(
-                              'Get Started',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                    SizedBox(height: 10),
+                    Text(
+                      'Welcome to our cozy coffe corner, where every cup is a delightful for you.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: LightTheme.textLightColor,
+                        // height: 1.5,
+                      ),
                     ),
-                  ),
+                    SizedBox(height: 20),
+                    MaterialButton(
+                      onPressed:
+                          () => Navigator.of(context).push(_createRoute()),
+                      minWidth: Size.infinite.width,
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      color: LightTheme.primaryColor,
+                      child: Text(
+                        'Get Started',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ],
       ),
