c3d5e88f6881eac4d1d331125dbedc3272c4a1a1
emmarp <ruiizemmanuel@gmail.com>
2025-03-14 11:36:32 -0600

    Refactor (CategoryCard): Remove Card widget

--- lib/src/screens/home/widgets/category_card_widget.dart
+++ lib/src/screens/home/widgets/category_card_widget.dart
@@ -1,6 +1,5 @@
 import 'package:coffe_shop_app/src/models/category_model.dart';
 import 'package:coffe_shop_app/src/themes/light_theme.dart';
-import 'package:coffe_shop_app/src/themes/theme.dart';
 import 'package:flutter/material.dart';
 
 class CategoryCardWidget extends StatelessWidget {
@@ -21,26 +20,19 @@ class CategoryCardWidget extends StatelessWidget {
       onTap: () => onCategorySelected(category.id),
       child: Container(
         margin: const EdgeInsets.only(right: 20),
-        child: Card(
-          color:
-              isSelected
-                  ? AppTheme.lightTheme.primaryColor
-                  : Color.fromRGBO(237, 237, 237, 0.35),
-          margin: const EdgeInsets.all(0),
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                category.name,
-                style: TextStyle(
-                  color: isSelected ? Colors.white : LightTheme.textColor,
-                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
-                  fontSize: 16,
-                ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: isSelected ? LightTheme.primaryColor : Color(0xFFEDEDED),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              category.name,
+              style: TextStyle(
+                color: isSelected ? Colors.white : LightTheme.textColor,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                fontSize: 16,
               ),
             ),
           ),
