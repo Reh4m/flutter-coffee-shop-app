026b8dee45aeb43cf8d5b1cad196665ad70f4591
emmarp <ruiizemmanuel@gmail.com>
2025-03-15 21:28:34 -0600

    Refactor (DeliveryScreen): Extract 'TopBar' and 'BottomSheet' into separated classes

--- lib/src/screens/delivery/delivery_screen.dart
+++ lib/src/screens/delivery/delivery_screen.dart
@@ -1,276 +1,30 @@
-import 'package:coffe_shop_app/src/themes/light_theme.dart';
-import 'package:expandable_bottom_sheet/expandable_bottom_sheet.dart';
+import 'package:coffe_shop_app/src/screens/delivery/widgets/bottom_sheet_widget.dart';
+import 'package:coffe_shop_app/src/screens/delivery/widgets/top_bar_widget.dart';
 import 'package:flutter/material.dart';
 
-class DeliveryScreen extends StatefulWidget {
+class DeliveryScreen extends StatelessWidget {
   const DeliveryScreen({super.key});
 
-  @override
-  State<DeliveryScreen> createState() => _DeliveryScreenState();
-}
-
-class _DeliveryScreenState extends State<DeliveryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
-          children: <Widget>[
-            Positioned(top: 0, left: 0, right: 0, child: _buildTopBar()),
-            ExpandableBottomSheet(
-              persistentContentHeight: 0,
-
-              background: Container(),
-
-              persistentHeader: Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Center(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.1,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: LightTheme.lightGrey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-
-              expandableContent: Container(
-                color: Colors.white,
-                child: _buildDeliveryDetails(),
-              ),
-            ),
-          ],
+          children: <Widget>[_buildTopBar(), BottomSheetWidget()],
         ),
       ),
     );
   }
 
-  Widget _buildDeliveryDetails() {
-    return Container(
-      margin: const EdgeInsets.only(top: 0, left: 20, right: 20),
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [
-          _buildTimeLeft(),
-          _buildDeliveryStatus(),
-          _buildDriverInfo(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTimeLeft() {
-    return Card(
-      color: Colors.transparent,
-      margin: EdgeInsets.zero,
-      elevation: 0,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            '10 minutes left',
-            style: TextStyle(
-              color: LightTheme.textColor,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 5),
-          RichText(
-            text: TextSpan(
-              style: const TextStyle(
-                color: LightTheme.textLightColor,
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-              ),
-              children: <TextSpan>[
-                TextSpan(text: 'Delivery to '),
-                TextSpan(
-                  text: 'John Doe',
-                  style: const TextStyle(
-                    color: LightTheme.textColor,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildDeliveryStatus() {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 20),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              for (int i = 0; i < 4; i++)
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: MediaQuery.of(context).size.width / 4 - 30,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: i == 3 ? LightTheme.lightGrey : Color(0xFF36C07E),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: LightTheme.lightGrey, width: 1),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(color: LightTheme.lightGrey, width: 1),
-                  ),
-                  child: const Icon(
-                    Icons.delivery_dining,
-                    color: LightTheme.primaryColor,
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Delivered your order',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        'We will deliver your goods to you in the shortes possible time.',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: LightTheme.textLightColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _buildTopBar() {
-    return Container(
-      margin: const EdgeInsets.only(top: 25, left: 20, right: 20),
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xFFEDEDED),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: const Icon(
-              Icons.arrow_back_ios,
-              color: LightTheme.textColor,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xFFEDEDED),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: const Icon(
-              Icons.gps_fixed_rounded,
-              color: LightTheme.textColor,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildDriverInfo() {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 20),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Image.asset(
-              'assets/coffee.jpg',
-              fit: BoxFit.cover,
-              height: 56,
-              width: 56,
-            ),
-          ),
-          const SizedBox(width: 20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Brooklyn Simmons',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'Personal Courier',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: LightTheme.textLightColor,
-                ),
-              ),
-            ],
-          ),
-          Spacer(),
-          Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: LightTheme.lightGrey, width: 1),
-            ),
-            child: const Icon(
-              Icons.phone_in_talk_outlined,
-              color: LightTheme.textColor,
-            ),
-          ),
-        ],
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        margin: const EdgeInsets.only(top: 25, left: 20, right: 20),
+        child: TopBarWidget(),
       ),
     );
   }
---
+++ lib/src/screens/delivery/widgets/top_bar_widget.dart
@@ -0,0 +1,28 @@
+import 'package:coffe_shop_app/src/themes/light_theme.dart';
+import 'package:flutter/material.dart';
+
+class TopBarWidget extends StatelessWidget {
+  const TopBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _iconContainer(Icons.arrow_back_ios),
+        _iconContainer(Icons.gps_fixed_rounded),
+      ],
+    );
+  }
+
+  Widget _iconContainer(IconData icon) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Color(0xFFEDEDED),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Icon(icon, color: LightTheme.textColor),
+    );
+  }
+}
