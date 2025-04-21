e1e2de25aa39c22da38db2b6d68beb05061bde2f
emmarp <ruiizemmanuel@gmail.com>
2025-04-21 15:47:45 -0600

    LightTheme: Add common colors used in the app

--- lib/src/themes/light_theme.dart
+++ lib/src/themes/light_theme.dart
@@ -5,14 +5,22 @@ class LightTheme {
   static const Color cardBackgroundColor = Color(0xFFFFFFFF);
 
   static const Color primaryColor = Color(0xFFC67C4E);
+  static const Color primaryLightColor = Color(0xFFF9F2ED);
   static const Color secondaryColor = Color(0xFFEDD6C8);
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
 
   static const Color textColor = Color(0xFF313131);
   static const Color textLightColor = Color(0xFFA2A2A2);
 
   static const Color lightGrey = Color(0xFFE3E3E3);
-  static const Color grey = Color(0xFFBDBDBD);
+  static const Color lightGrey2 = Color(0xFFEDEDED);
+  static const Color lightGrey3 = Color(0xFFD8D8D8);
+
   static const Color darkGrey = Color(0xFF313131);
+  static const Color secondaryTextColor = Color(0xFF242424);
+  static const Color darkGrey3 = Color(0xFF2A2A2A);
 
   static const Color errorColor = Color(0xFFED5151);
+  static const Color successColor = Color(0xFF36C07E);
+  static const Color warningColor = Color(0xFFFBBE21);
 }
