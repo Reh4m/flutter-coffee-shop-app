79e6547669e17a05036c598ce78a2f2245c4be18
emmarp <ruiizemmanuel@gmail.com>
2025-06-30 09:50:57 -0600

    Feat (Widgets): Primary Button global widget

---
+++ lib/src/widgets/primary_button_widget.dart
@@ -0,0 +1,42 @@
+import 'package:coffe_shop_app/src/themes/light_theme.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final double width;
+  final double height;
+
+  const PrimaryButtonWidget({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.width,
+    this.height = 56,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: FilledButton(
+        onPressed: onPressed,
+        style: FilledButton.styleFrom(
+          backgroundColor: LightTheme.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
