0c28c30c5ee603b3af1fcd10901c7894f9098b6c
emmarp <ruiizemmanuel@gmail.com>
2025-06-30 09:09:16 -0600

    Refactor: Update Main class

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:coffe_shop_app/src/config/routes.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const CoffeeShopApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeeShopApp extends StatelessWidget {
+  const CoffeeShopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Coffee Shop Template',
       theme: AppTheme.lightTheme.copyWith(
-        textTheme: GoogleFonts.soraTextTheme(Theme.of(context).textTheme),
+        textTheme: GoogleFonts.soraTextTheme(),
       ),
       onGenerateRoute: Routes.generateRoute,
       initialRoute: '/get-started',
